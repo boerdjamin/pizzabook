fabc8529061e9f7c0dade2dd78a3eec5e4cfdf69
Birte Kock <24297798+boerdjamin@users.noreply.github.com>
2021-09-16 18:19:31 +0200

    Added pizza details

--- src/screens/pizza-details/pizza-details.tsx
+++ src/screens/pizza-details/pizza-details.tsx
@@ -1,10 +1,11 @@
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { appTexts } from '../../data/texts';
 import { Pizza } from '../../models/pizza';
 import { RootStackParamList, Routes } from '../../navigation/root-navigation';
-import { Colors } from '../../styles';
+import { Colors, Spacing, textStyles } from '../../styles';
 
 interface PizzaDetailsScreenProps {
   readonly pizza: Pizza;
@@ -19,22 +20,36 @@ type NavigationProp = StackNavigationProp<RootStackParamList, Routes.Details>;
 
 const PizzaDetailsScreen: React.FC<PizzaDetailsScreenProps> = () => {
   const { setOptions } = useNavigation<NavigationProp>();
-  const {
-    params: { pizza },
-  } = useRoute<PizzaDetailsScreenRouteProp>();
+  const route = useRoute<PizzaDetailsScreenRouteProp>();
+  const { name, toppings, photo, isVegan, canBeVeganized } = route.params.pizza;
 
   React.useLayoutEffect(
     () =>
       setOptions({
-        title: pizza.name,
+        title: name,
         headerStyle: { borderWidth: 1 },
       }),
-    [pizza, setOptions],
+    [name, setOptions],
   );
 
   return (
     <View style={styles.container}>
-      <Text>{pizza.name}</Text>
+      <View style={styles.imageContainer} />
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.ingridientsList}>
+        {toppings.map(
+          (topping, i) =>
+            `${topping.name + (i === toppings.length - 1 ? '' : ', ')}`,
+        )}
+      </Text>
+      {photo ? <Image source={photo} /> : null}
+      <Text style={styles.veganMarker}>
+        {isVegan
+          ? appTexts.vegan
+          : canBeVeganized
+          ? appTexts.veganPossible
+          : appTexts.vegetarian}
+      </Text>
     </View>
   );
 };
@@ -45,5 +60,20 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.white,
     flex: 1,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: '80%',
+    height: 250,
+    borderWidth: 1,
+    margin: Spacing.medium,
+  },
+  title: {},
+  ingridientsList: {
+    ...textStyles.p1,
+    marginBottom: Spacing.smaller,
+  },
+  veganMarker: {
+    color: Colors.success,
   },
 });
